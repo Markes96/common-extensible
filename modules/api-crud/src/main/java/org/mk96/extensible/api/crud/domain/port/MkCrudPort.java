Markes96/extensible

Project extensible 1st comit

modules/api-crud/src/main/java/org/mk96/extensible/api/crud/domain/port/MkCrudPort.java
@@ -1,0 +1,8 @@
+package org.mk96.extensible.api.crud.domain.port;
+
+import org.mk96.extensible.api.crud.domain.example.port.MkExampleCrudPort;
+import org.mk96.extensible.api.crud.domain.id.port.MkIdCrudPort;
+import org.mk96.extensible.api.domain.model.MkDomain;
+
+public interface MkCrudPort<DOM extends MkDomain, ID>
+	extends MkExampleCrudPort<DOM>, MkIdCrudPort<DOM, ID> {}

modules/api-crud/src/main/java/org/mk96/extensible/api/crud/domain/example/port/MkExampleCrudPort.java
@@ -1,0 +1,10 @@
+package org.mk96.extensible.api.crud.domain.example.port;
+
+import org.mk96.extensible.api.crud.domain.port.MkCreatePort;
+import org.mk96.extensible.api.domain.model.MkDomain;
+
+public interface MkExampleCrudPort<DOM extends MkDomain>
+	extends MkCreatePort<DOM>,
+	MkExampleUpdatePort<DOM>,
+	MkExampleFindPort<DOM>,
+	MkExampleDeletePort<DOM> {}

modules/api-crud/src/main/java/org/mk96/extensible/api/crud/domain/example/service/MkExampleCrudService.java
@@ -1,0 +1,10 @@
+package org.mk96.extensible.api.crud.domain.example.service;
+
+import org.mk96.extensible.api.crud.domain.service.MkCreateService;
+import org.mk96.extensible.api.domain.model.MkDomain;
+
+public interface MkExampleCrudService<DOM extends MkDomain>
+	extends MkCreateService<DOM>,
+	MkExampleUpdateService<DOM>,
+	MkExampleFindService<DOM>,
+	MkExampleDeleteService<DOM> {}

Markes96/common-extensible

modules/api-crud/src/main/java/org/mk96/extensible/api/crud/domain/port/MkCrudPort.java
@@ -1,0 +1,8 @@
+package org.mk96.extensible.api.crud.domain.port;
+
+import org.mk96.extensible.api.crud.domain.port.example.MkExampleCrudPort;
+import org.mk96.extensible.api.crud.domain.port.id.MkIdCrudPort;
+import org.mk96.extensible.api.domain.model.MkDomain;
+
+public interface MkCrudPort<DOM extends MkDomain, ID>
+	extends MkExampleCrudPort<DOM>, MkIdCrudPort<DOM, ID> {}

modules/api-crud/src/main/java/org/mk96/extensible/api/crud/domain/port/example/MkExampleCrudPort.java
@@ -1,0 +1,9 @@
+package org.mk96.extensible.api.crud.domain.port.example;
+
+import org.mk96.extensible.api.domain.model.MkDomain;
+
+public interface MkExampleCrudPort<DOM extends MkDomain>
+	extends MkExampleCreatePort<DOM>,
+	MkExampleUpdatePort<DOM>,
+	MkExampleFindPort<DOM>,
+	MkExampleDeletePort<DOM> {}

modules/api-crud/src/main/java/org/mk96/extensible/api/crud/domain/service/example/MkExampleCrudService.java
@@ -1,0 +1,9 @@
+package org.mk96.extensible.api.crud.domain.service.example;
+
+import org.mk96.extensible.api.domain.model.MkDomain;
+
+public interface MkExampleCrudService<DOM extends MkDomain>
+	extends MkExampleCreateService<DOM>,
+	MkExampleUpdateService<DOM>,
+	MkExampleFindService<DOM>,
+	MkExampleDeleteService<DOM> {}